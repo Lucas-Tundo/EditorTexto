38770dedf71e1edf14f8265497009d1eab722c0b
Lucas Tundo <lucastundo@gmail.com>
2025-05-16 15:20:56 -0300

    Initial commit

--- obj/Debug/net9.0/EditorTexto.AssemblyInfo.cs
+++ obj/Debug/net9.0/EditorTexto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EditorTexto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63768330b599c72683f5c741105149412ef0e806")]
 [assembly: System.Reflection.AssemblyProductAttribute("EditorTexto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EditorTexto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
